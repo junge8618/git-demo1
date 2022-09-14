ee9f5518b6bbbfbb4b0e668850ed86d6ceda938a
junge8618 <513851613@qq.com>
2022-09-14 20:11:54 +0800

    第三次提交

--- src/main/java/com/junge/demo/git/GitHello.java
+++ src/main/java/com/junge/demo/git/GitHello.java
@@ -3,5 +3,6 @@ package com.junge.demo.git;
 public class GitHello {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
